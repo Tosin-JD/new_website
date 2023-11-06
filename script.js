60700f52441aeb743985175522a3098475381887
Oluwatosin  Joseph Durodola <oluwatosinjosephdurodola@gmail.com>
2023-11-06 16:44:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.body.addEventListener('click', createStars);
 
+        function createStars(event) {
+            const starCount = 30; // Number of stars to create
+            for (let i = 0; i < starCount; i++) {
+                const star = document.createElement('div');
+                star.className = 'star';
+                star.style.left = event.clientX + 'px';
+                star.style.top = event.clientY + 'px';
+                star.style.animation = `twinkle ${Math.random() * 2 + 1}s linear`;
+                document.body.appendChild(star);
+
+                // Remove the star element after the animation
+                star.addEventListener('animationend', () => {
+                    star.remove();
+                });
+            }
+        }
